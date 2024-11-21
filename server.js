d30a52f8a108c02912567142e9b225355060a2f9
OsherElikamel <128866114+OsherElikamel@users.noreply.github.com>
2024-11-21 21:51:28 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const { Server } = require('socket.io');
 const connectDB = require('./config/db');
 const app = require('./app');
 const socketHandler = require('./socket');
-const path = require('path');
 
 const PORT = process.env.PORT || 3001;
 
@@ -15,11 +14,6 @@ const io = new Server(server, {
   cors: { origin: '*', methods: ['GET', 'POST'] },
 });
 
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
 socketHandler(io);
 
 server.listen(PORT, () => {
