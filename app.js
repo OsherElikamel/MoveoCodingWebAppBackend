51916297214920ea873137587d70e02d8728dd5f
OsherElikamel <128866114+OsherElikamel@users.noreply.github.com>
2024-11-21 21:51:55 +0100

    Update app.js

--- app.js
+++ app.js
@@ -8,5 +8,10 @@ app.use(cors({ origin: '*' }));
 app.use(express.json());
 
 app.use('/api/codeblocks', codeBlockRoutes);
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 module.exports = app;
