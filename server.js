567b4c277170f3205a4cbdadb30ce6286f84b344
OsherElikamel <128866114+OsherElikamel@users.noreply.github.com>
2024-11-21 21:45:34 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const { Server } = require('socket.io');
 const connectDB = require('./config/db');
 const app = require('./app');
 const socketHandler = require('./socket');
+const path = require('path');
 
 const PORT = process.env.PORT || 3001;
 
@@ -14,8 +15,13 @@ const io = new Server(server, {
   cors: { origin: '*', methods: ['GET', 'POST'] },
 });
 
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 socketHandler(io);
 
 server.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
